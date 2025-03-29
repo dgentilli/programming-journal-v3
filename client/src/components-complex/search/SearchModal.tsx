11f8e63668401f54e22b5b471cd1453b39334310
Dave Gentilli <dgentilli@gmail.com>
2025-03-29 17:17:37 +0200

    Extract modal into a separate component that wraps modal content
    To do: add a style prop and style for small screens

--- client/src/components-complex/search/SearchModal.tsx
+++ client/src/components-complex/search/SearchModal.tsx
@@ -1,54 +1,10 @@
-import { styled, keyframes } from 'styled-components';
-import { BaseTokens, baseTokens } from '../../theme/baseTokens';
+import { styled } from 'styled-components';
+import { baseTokens } from '../../theme/baseTokens';
 import Spacer from '../../components-simple/Spacer';
-import CustomLink from '../../components-simple/CustomLink';
 import SearchInput from '../../components-simple/SearchInput';
 import { Journal } from '../../types/common';
 import SearchResults from '../../components-simple/SearchResults';
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
-const slideIn = keyframes`
-  from {
-    transform: translateY(-50px);
-    opacity: 0;
-  }
-  to {
-    transform: translateY(0);
-    opacity: 1;
-  }
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  animation: ${fadeIn} 0.3s ease;
-`;
-
-const Wrapper = styled.div`
-  width: 50%;
-  height: 600px;
-  padding: ${baseTokens.spacing.md};
-  background-color: ${baseTokens.colors.white};
-  border-radius: ${baseTokens.radius.md};
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-  animation: ${slideIn} 0.3s ease;
-  overflow-y: auto;
-`;
+import Modal from '../../components-simple/Modal';
 
 const Message = styled.p`
   font-size: ${baseTokens.fontSizes.xxl};
@@ -97,36 +53,28 @@ const SearchModal = (props: SearchModalProps) => {
   const { searchResults, totalCount, totalPages, currentPage } = data || {};
 
   return (
-    <Overlay>
-      <Wrapper>
-        <Message>Search for a journal entry</Message>
-        <CustomLink
-          title='Close'
-          color={baseTokens.colors.blue500}
-          fontSize={baseTokens.fontSizes.md as keyof BaseTokens['fontSizes']}
-          onClick={closeModal}
-        />
-        <Spacer height={baseTokens.spacing.xl} />
-        <SearchInput
-          value={query}
-          onChange={(input: string) => setQuery(input)}
-        />
-        <Spacer height={baseTokens.spacing.xl} />
-        <SearchResults
-          searchResults={searchResults}
-          isLoading={isLoading}
-          isError={isError}
-          error={error}
-          query={query}
-          totalCount={totalCount}
-          currentPage={currentPage}
-          totalPages={totalPages}
-          goToNextPage={goToNextPage}
-          goToPreviousPage={goToPreviousPage}
-          onClickListItem={onClickListItem}
-        />
-      </Wrapper>
-    </Overlay>
+    <Modal closeModal={closeModal}>
+      <Message>Search for a journal entry</Message>
+      <Spacer height={baseTokens.spacing.xl} />
+      <SearchInput
+        value={query}
+        onChange={(input: string) => setQuery(input)}
+      />
+      <Spacer height={baseTokens.spacing.xl} />
+      <SearchResults
+        searchResults={searchResults}
+        isLoading={isLoading}
+        isError={isError}
+        error={error}
+        query={query}
+        totalCount={totalCount}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        goToNextPage={goToNextPage}
+        goToPreviousPage={goToPreviousPage}
+        onClickListItem={onClickListItem}
+      />
+    </Modal>
   );
 };
 
---
+++ client/src/components-simple/Modal.tsx
@@ -0,0 +1,75 @@
+import { styled, keyframes } from 'styled-components';
+import { BaseTokens, baseTokens } from '../theme/baseTokens';
+import React from 'react';
+import CustomLink from './CustomLink';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateY(-50px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${fadeIn} 0.3s ease;
+`;
+
+const Wrapper = styled.div`
+  width: 50%;
+  height: 600px;
+  padding: ${baseTokens.spacing.md};
+  background-color: ${baseTokens.colors.white};
+  border-radius: ${baseTokens.radius.md};
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  animation: ${slideIn} 0.3s ease;
+  overflow-y: auto;
+`;
+
+interface SearchModalProps {
+  closeModal: () => void;
+  children: React.ReactElement[];
+  // to do -- implement a style prop to adjust height and width
+  modalWrapperStyle?: { height: string; width: string };
+}
+
+const Modal = (props: SearchModalProps) => {
+  const { closeModal, children } = props;
+
+  return (
+    <Overlay>
+      <Wrapper>
+        <CustomLink
+          title='Close'
+          color={baseTokens.colors.blue500}
+          fontSize={baseTokens.fontSizes.md as keyof BaseTokens['fontSizes']}
+          onClick={closeModal}
+        />
+        {children}
+      </Wrapper>
+    </Overlay>
+  );
+};
+
+export default Modal;
