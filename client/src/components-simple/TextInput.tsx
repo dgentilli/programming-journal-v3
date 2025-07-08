4c18d011db76ec2db26bd321a037ab78c5c0402c
Dave Gentilli <dgentilli@gmail.com>
2025-07-08 21:02:16 +0300

    Add onto login / signup

--- client/src/components-simple/TextInput.tsx
+++ client/src/components-simple/TextInput.tsx
@@ -1,32 +1,65 @@
 import Input from '@mui/joy/Input';
 import FormLabel from '@mui/joy/FormLabel';
-
-interface TextInputProps {
+import Typography from '@mui/joy/Typography';
+import { InputHTMLAttributes } from 'react';
+import { ColorPaletteProp } from '@mui/joy';
+interface TextInputProps
+  extends Omit<
+    InputHTMLAttributes<HTMLInputElement>,
+    'name' | 'value' | 'onChange'
+  > {
   name: string;
   value: string;
   placeholder?: string;
   label?: string;
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  errorMessage?: string;
+  isRequired?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
-
+/** name, value, and onChange are actually optional on the HTMLInputElement
+ * So omit them, then add them in manually as required props
+ */
 const TextInput = (props: TextInputProps) => {
   const {
     name,
     value,
     placeholder = 'Type in here…',
     label = '',
-    handleChange,
+    errorMessage,
+    isRequired,
+    onChange,
+    ...rest
   } = props;
+
+  const inputColor: ColorPaletteProp = errorMessage ? 'danger' : 'neutral';
+
+  const renderErrorMessage = () => {
+    if (errorMessage) {
+      return (
+        <Typography level='body-md' color='danger' sx={{ mt: 0.5 }}>
+          {errorMessage}
+        </Typography>
+      );
+    }
+
+    return <></>;
+  };
 
   return (
     <div>
-      <FormLabel>{label || name}</FormLabel>
+      <FormLabel required={isRequired}>
+        <Typography level='body-md'>{label || name}</Typography>
+      </FormLabel>
       <Input
         placeholder={placeholder}
         value={value}
         name={name}
-        onChange={handleChange}
+        onChange={onChange}
+        //@ts-expect-error this uses the ColorPaletteProp from mui
+        color={inputColor}
+        {...rest}
       />
+      {renderErrorMessage()}
     </div>
   );
 };
--- client/src/screens/LoginSignup/LoginSignupUI.tsx
+++ client/src/screens/LoginSignup/LoginSignupUI.tsx
@@ -1,4 +1,6 @@
+import Spacer from '../../components-simple/Spacer';
 import TextInput from '../../components-simple/TextInput';
+import { baseTokens } from '../../theme/baseTokens';
 
 interface Props {
   formData: { email: string; password: string };
@@ -9,13 +11,24 @@ const LoginSignupUI = (props: Props) => {
   const { formData, handleChange } = props;
 
   return (
-    <TextInput
-      name='email'
-      label='Email'
-      value={formData.email}
-      placeholder='Email'
-      handleChange={handleChange}
-    />
+    <>
+      <TextInput
+        name='email'
+        label='Email'
+        value={formData.email}
+        placeholder='Email'
+        onChange={handleChange}
+      />
+      <Spacer height={baseTokens.spacing.xxl} />
+      <TextInput
+        name='password'
+        label='Password'
+        type='password'
+        value={formData.password}
+        placeholder='Password'
+        onChange={handleChange}
+      />
+    </>
   );
 };
 
